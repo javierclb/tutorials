4d255384e4ea7e75d71216383d65a3333e1c69a7
Arnav Sood <soodarnav01@gmail.com>
2018-09-27 21:52:35 -0700

    warn() => @warn

--- etc/setup.jl
+++ etc/setup.jl
@@ -1,17 +1,17 @@
 #Run this file a new installation.  Hopefully all unnecessary with Pkg3
 
 # Adding important libraries.
-warn("Installing important libraries.  Be extremely patient")
+@warn "Installing important libraries.  Be extremely patient"
 using Pkg 
 pkg"add Revise Plots QuantEcon Parameters Expectations IJulia GR NLsolve Interpolations"
 
 # Updating libraries. 
-warn("The package updates and precompilation will be slow!")
+@warn "The package updates and precompilation will be slow!"
 pkg"up"
 
 #Precompiling installed libraries
-warn("Precompiling libraries. Be even more patient")
+@warn "Precompiling libraries. Be even more patient"
 pkg"precompile"
 
-warn("For setting up editors the Julia Binary location is at")
+@warn "For setting up editors the Julia Binary location is at"
 Sys.BINDIR
